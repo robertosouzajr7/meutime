b6afcd334cde7072bfabc598a14b5f18f8f48a06
Roberto Jr <roberto-santos07@hotmail.com>
2023-05-17 02:39:26 -0300

    feat: create all pasts and struture for development

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import GlobalStyle from "./styles/globalStyles";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={} />
+      </Routes>
+      <GlobalStyle />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Main/style.ts
@@ -0,0 +1,7 @@
+import { styled } from "styled-components";
+
+export const MainStayled = styled.main`
+  display: flex;
+  width: max-content;
+  background-color: #d9d9d9;
+`;
